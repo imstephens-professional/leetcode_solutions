c54371ead2659be05ed8ef00349155a3db4ad383
Isabella Stephens <imstephens.professional@gmail.com>
2026-07-25 23:03:25 -0400

    Create SortColors_BF.java

---
+++ Medium/75. Sort Colors/SortColors_BF.java
@@ -0,0 +1,11 @@
+//  3 ms - beats 14%
+// 43.80 mb - beats 11.12%
+
+class Solution {
+    public void sortColors(int[] nums) {
+        // red = 0
+        // white = 1
+        // blue = 2
+        Arrays.sort(nums);
+    }
+}
